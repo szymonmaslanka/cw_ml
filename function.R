62745efda54a98584fb8570b75f331159c5204fe
Szymon Maślanka <smaslanka@student.agh.edu.pl>
2025-05-27 18:37:29 +0200

    File and gitignore change

---
+++ function.R
@@ -0,0 +1,37 @@
+# funkcja liczy parametr y na podstawie zbioru danych x
+# x - zbiór danych w postaci wektora liczbowego 
+# funkcja sprawdza, czy zmienna jest typu numerycznego 
+
+new_f <- function(x){
+  
+  if (!is.numeric(x)) { stop("Obiekt wejściowy x nie jest typu numeric") }
+  
+  srednia <- mean(x)
+  
+  y <- srednia / 10 + sqrt(srednia)
+  
+  return(y)
+  
+}
+
+# test 1 funkcji 
+
+dane <- 1:10
+
+new_f(x = dane)
+
+# test 2 - sprawdzamy, czy odporna
+
+new_f(x = letters)
+
+letters |> str()
+
+ggplot(dane, aes(x = x, y = y)) +
+  geom_point(color = "blue", size = 3) +
+  geom_line(color = "blue") +
+  labs(
+    title = "Wykres y = x/10 + sqrt(x)",
+    x = "x",
+    y = "y"
+  ) +
+  theme_minimal()
